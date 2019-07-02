b94f1d9e64132b6e02ac978e3024eab7c19e2dd8
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2019-07-02 16:20:18 +0200

    General update

--- Territory.py
+++ Territory.py
@@ -1,16 +1,23 @@
-import geopandas as gp
 import pandas as pd
 import random
 
 
-class Territory(gp.GeoSeries):
+class Territory(pd.Series):
     @property
     def _constructor(self):
         return Territory
 
     @property
-    def _constructor_sliced(self):
+    def _constructor_expanddim(self):
         return Territory
+
+    @property
+    def SOVRANO(self):
+        return self.name[0]
+
+    @property
+    def COMUNE(self):
+        return self.name[1]
 
     @staticmethod
     def attack():
@@ -21,31 +28,49 @@ class Territory(gp.GeoSeries):
         return random.random()
 
 
-class Reign(gp.GeoDataFrame):
+@pd.api.extensions.register_dataframe_accessor('reign')
+class Reign(object):
+
+    def __init__(self, pandas_obj):
+        self.obj = pandas_obj
+        # self.find_neighbors(pandas_obj)
 
     @property
-    def _constructor(self):
-        return Reign
+    def SOVRANI(self):
+        return self.obj.index.get_level_values(0).tolist()
 
     @property
-    def _constructor_sliced(self):
-        return Reign
+    def COMUNI(self):
+        return self.obj.index.get_level_values(1).tolist()
 
     def find_neighbors(self):
         neighbours = []
-        for i, territory in self.iterrows():
-            n = self[~self.geometry.disjoint(territory.geometry)].COMUNE.tolist()
-            neighbours.append([name for name in n if territory.COMUNE != name])
-        self["neighbours"] = neighbours
+        for i, territory in self.obj.iterrows():
+            territory = Territory(territory)
+            n = self.obj[~self.obj.geometry.disjoint(territory.geometry)].reign.SOVRANI
+            neighbours.append([name for name in n if territory.name[0] != name])
+        self.obj["extended_neighbours"] = neighbours
 
     def battle(self):
-        attacker: Territory = self.sample(1)[0]
-        defender: Territory = self[random.choice(attacker.neighbours)]
+        attacker = Territory(self.obj.sample(1).iloc[0])
+        defender = Territory(self.obj.loc[random.choice(attacker.extended_neighbours)])
+
+        print(f"{attacker.COMUNE} is attacking {defender.COMUNE}... ⚔️")
 
         if attacker.attack() > defender.defend():
-            self.drop(index=defender.index)
-            self.iloc[attacker.index].geometry = self.iloc[attacker.index].geometry.union(defender.geometry)
-            print(f"{attacker.COMUNE} won")
+
+            self.obj.drop(defender.COMUNE, inplace=True)
+            self.obj.loc[attacker.COMUNE].geometry = self.obj.loc[attacker.COMUNE].geometry.union(defender.geometry)
+            print(f"{attacker.COMUNE} conquered {defender.COMUNE} 🗡")
+            print(f"{len(self.obj)} remaining territories.\n")
             self.find_neighbors()
         else:
-            print(f"{defender.COMUNE} resisted")
+            print(f"{defender.COMUNE} resisted to the attack of {attacker.COMUNE} 🛡\n")
+
+    @staticmethod
+    def attack():
+        return random.random()
+
+    @staticmethod
+    def defend():
+        return random.random()
--- app2.py
+++ app2.py
@@ -11,7 +11,18 @@ from functools import reduce
 df = pd.read_pickle("bologna.pickle")
 
 
-df = Reign(df)
-df.find_neighbors()
+reign = Reign(df)
+reign.obj["extended_geometry"] = reign.obj.geometry
+#reign.obj["SOVRANO"] = reign.obj.geometry
+index = pd.MultiIndex.from_arrays([reign.obj.COMUNE, reign.obj.COMUNE], names=["SOVRANO", "COMUNE"])
+
+reign.obj.index = index
+reign.obj.drop(columns=["COMUNE"], inplace=True)
+reign.find_neighbors()
 
 
+while len(reign.obj) > 1:
+    reign.battle()
+    # sleep(1)
+
+print(f"🏆🏆🏆 {reign.obj.iloc[0].COMUNE.upper()} WON THE WAR!!! 🏆🏆🏆")
