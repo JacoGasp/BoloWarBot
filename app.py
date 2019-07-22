8da14f6fcd8314fbec97afd1b652ff9e3daee5d5
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2019-07-22 15:32:30 +0200

    Refactor: all dataframe preparation moved to finde_city_shape_from_istat

--- app.py
+++ app.py
@@ -4,13 +4,6 @@ logger = logging.getLogger("Reign")
 logger.setLevel("DEBUG")
 
 df = pd.read_pickle("bologna.pickle")
-df["empire_geometry"] = df.geometry
-df["empire_neighbours"] = df.neighbours
-df["empire_color"] = df.color
-
-df = df.rename(columns={"COMUNE": "Territory"})
-df["Empire"] = df.Territory
-df.set_index("Territory", drop=True, inplace=True)
 
 reign = Reign(df)
 
--- find_city_shape_from_istat.py
+++ find_city_shape_from_istat.py
@@ -1,20 +1,31 @@
 import geopandas as gp
 import matplotlib.pyplot as plt
 
-df = gp.read_file("Com01012019_g_WGS84.shp")
+df = gp.read_file("Dati_istat/Com01012019_g_WGS84.shp")
 df.head()
 
-bolo = df[df.COD_PROV == 37][["COMUNE", "geometry"]].reset_index(drop=True)
+df = df[df.COD_PROV == 37][["COMUNE", "geometry"]].reset_index(drop=True)
 
 neighbours = []
 
 cm = plt.get_cmap('RdBu')
 
-for i, comune in bolo.iterrows():
-    tmp_neighbours = bolo[~bolo.geometry.disjoint(comune.geometry)].COMUNE.tolist()
+for i, comune in df.iterrows():
+    tmp_neighbours = df[~df.geometry.disjoint(comune.geometry)].COMUNE.tolist()
     neighbours.append([name for name in tmp_neighbours if comune.COMUNE != name])
-bolo["neighbours"] = neighbours
 
-bolo["color"] = [cm(1. * i / len(bolo), alpha=0.75) for i in range(len(bolo))]
-bolo.head()
-bolo.to_pickle("Bologna.pickle")
+
+df["neighbours"] = neighbours
+df["color"] = [cm(1. * i / len(df), alpha=0.75) for i in range(len(df))]
+
+df["empire_geometry"] = df.geometry
+df["empire_neighbours"] = df.neighbours
+df["empire_color"] = df.color
+
+df = df.rename(columns={"COMUNE": "Territory"})
+df["Empire"] = df.Territory
+
+df.set_index("Territory", drop=True, inplace=True)
+df.head()
+
+df.to_pickle("Bologna.pickle")
